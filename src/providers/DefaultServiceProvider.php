c0a6652def0233cca52405235e9c12d433551aa4
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2024-12-16 15:07:39 +0200

    Changes

--- src/providers/DefaultServiceProvider.php
+++ src/providers/DefaultServiceProvider.php
@@ -7,13 +7,13 @@ use Illuminate\Support\ServiceProvider;
 
 class DefaultServiceProvider extends ServiceProvider{
 
+    
     public function boot(){
 
     }
 
-
     public function register(){
-        
+
     }
 
 }
