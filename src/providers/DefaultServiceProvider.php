a80cdcf5777a8fe5b8ad05d6c67c0d28bc23b010
Mahdi Moukaddem <mahdi.70.023@gmail.com>
2024-12-16 15:03:56 +0200

    first commit

---
+++ src/providers/DefaultServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace twa\cmsv2\providers;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class DefaultServiceProvider extends ServiceProvider{
+
+    public function boot(){
+
+    }
+
+
+    public function register(){
+        
+    }
+
+}
