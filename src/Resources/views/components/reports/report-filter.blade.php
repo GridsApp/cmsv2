8f5f0824e87f8fde3d0e64e547ba03b982c99509
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2025-05-07 12:50:56 +0300

    update

--- src/Resources/views/components/reports/report-filter.blade.php
+++ src/Resources/views/components/reports/report-filter.blade.php
@@ -1,6 +1,6 @@
-<form class="card" wire:submit.prevent="applyFilters" x-data="GeneralFunctions.filterForm()"
+<form class="card card-body" wire:submit.prevent="applyFilters" x-data="GeneralFunctions.filterForm()"
     x-on:query-completed.window="disabled = false" x-on:start-report.window="disabled = true">
-    <div class="card-body grid grid-cols-12 gap-3 card-body-rounded-bottom ">
+    <div class=" grid grid-cols-12 gap-3 card-body-rounded-bottom ">
         @foreach ($class->filters as $filter)
             {!! field($filter) !!}
             @if ($filter['name'] === 'date')
@@ -11,14 +11,17 @@
         @endforeach
 
         {{-- <div class="col-span-12"> --}}
-        <div class="col-span-6 mt-10">
+   
+        {{-- </div> --}}
+    </div>
+    <div class="flex gap-4">
+        <div class=" w-full mt-10">
             <a href="{{ route('show-report', ['slug' => $slug]) }}" class="btn bg-gray-300 text-white w-full">Clear
                 filters</a>
         </div>
-        <div class="col-span-6 mt-10">
+        <div class=" w-full mt-10">
             <button :disabled="disabled" class="btn btn-primary w-full">Refine</button>
         </div>
-        {{-- </div> --}}
-    </div>
+      </div>
 
 </form>
